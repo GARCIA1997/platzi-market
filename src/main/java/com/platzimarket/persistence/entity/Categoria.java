e57be163cf9ec57b9a49a1580aa5a03731259550
Alejandro Garcia <garciaalvale@gmail.com>
2021-10-06 12:01:12 -0500

    Creacion de tablas y relaciones de las bases de datos peero en spring "MODLEOS" Aqui nombrados entity

---
+++ src/main/java/com/platzimarket/persistence/entity/Categoria.java
@@ -0,0 +1,46 @@
+package com.platzimarket.persistence.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+    //Declaramos Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+    //Declaramos las columnas
+    private String descripcion;
+    private Boolean estado;
+    //Declaramos la relacion uno a muchos de categoria
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> producto;
+
+
+    //Declaramos G AND S
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/platzimarket/persistence/entity/ComprasProducto.java
@@ -0,0 +1,59 @@
+package com.platzimarket.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+    //LLAVE PRIMARIA COMPUESTA
+    @EmbeddedId
+    private ComprasProductoPK id;
+    //Datos de la tabla
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
+
+
+    //Relaciones a llaves Foraneas Este es un detalle por eso tiene dos relaciones
+    //Relacion de Compra a ComprasProducto
+    @ManyToOne
+    @JoinColumn (name = "id_compra", insertable = false, updatable = false)
+    private Compra compra;
+    //Relacion de Producto a ComprasPoroducto
+    @ManyToOne
+    @JoinColumn (name = "id_producto", insertable = false, updatable = false)
+    private Producto producto;
+
+    //Generacion de Getters and Setters
+    public ComprasProductoPK getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
