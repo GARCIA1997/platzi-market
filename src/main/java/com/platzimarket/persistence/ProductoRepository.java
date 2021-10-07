1bf36254239b9d3956987a6638103d555c74b5f5
Alejandro Garcia <garciaalvale@gmail.com>
2021-10-07 09:02:32 -0500

    Servicio expuesto

--- src/main/java/com/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzimarket/persistence/ProductoRepository.java
@@ -5,14 +5,18 @@ import com.platzimarket.domain.repository.ProductRepository;
 import com.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzimarket.persistence.entity.Producto;
 import com.platzimarket.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
     private ProductMapper mapper;
+
     @Override
     public List<Product> getAll(){
         List<Producto> productos =(List<Producto>) productoCrudRepository.findAll();
